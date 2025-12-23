0410be8bc55df74636cd9abcdf049a712781ebd8
FelipeSantosTech <felpsantos.c@gmail.com>
2025-12-23 11:38:18 -0300

    Initial Commit

---
+++ IrrevHabit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IrrevHabit
+//
+//  Created by Paulo Marcelo Santos on 23/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IrrevHabit/IrrevHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrrevHabitApp.swift
+//  IrrevHabit
+//
+//  Created by Paulo Marcelo Santos on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct IrrevHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

522e9416fe79c1a19b8ae3b6ef862630818ddd4e
FelipeSantosTech <felpsantos.c@gmail.com>
2025-12-23 17:02:43 -0300

    Added the first lines of code,z and vstack, changed background color added some text

--- IrrevHabit/ContentView.swift
+++ IrrevHabit/ContentView.swift
@@ -1,24 +1,26 @@
-//
 //  ContentView.swift
 //  IrrevHabit
-//
 //  Created by Paulo Marcelo Santos on 23/12/25.
-//
-
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello,I'm building my first app!!")
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                VStack(spacing:12){
+                    Text("IrrevHabit does not motivate you")
+                    Text("It records what you actually do")
+                    Text("Misses are permanent")
+                }
+                .foregroundColor(.white)
+                .font(.system(.body, design: .monospaced))
+                Spacer()
+            }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
